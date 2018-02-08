1d3f789ad13ba00f4511162c9d8f0c06ca29241e
bobo <bobo770880@163.com>
2018-02-08 17:55:30 +0800

    prerender

---
+++ src/components/About.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="about">
+    <p>{{msg}}</p>
+    <a @click.prevent="$router.push({name:'contact'})">go联系我们</a>
+    <ul>
+      <li v-for="item in list">{{item.title}}</li>
+    </ul>
+  </div>
+</template>
+<script>
+export default {
+  name: 'about',
+  data() {
+    return {
+      msg: '这里是关于我们的页面',
+      list: []
+    }
+  },
+  created() {
+    this.getData();
+  },
+  methods: {
+    getData() {
+      this.$http.get('/static/list.json').then((res) => {
+        this.list=res.data.books;
+      })
+    }
+  }
+}
+</script>
---
+++ src/components/Contact.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="contact">
+    <p>{{msg}}</p>
+    <a @click.prevent="$router.push({name:'about'})">go关于我们</a>
+    <ul>
+      <img :src="info.image" :title="info.title" />
+      <h1>描述：</h1>
+        <p>{{info.summary}}</p>
+    </ul>
+  </div>
+</template>
+<script>
+export default {
+  name: 'about',
+  data() {
+    return {
+      msg: '这里是联系我们的页面',
+      info: []
+    }
+  },
+  created() {
+    this.getData();
+  },
+  methods: {
+    getData() {
+      this.$http.get('/static/info.json').then((res) => {
+        this.info = res.data;
+      })
+    }
+  }
+}
+
+</script>
